d83564dc8610cac11e1a08306b5b8e6051dd190f
Oleksii1994 <112803852+Oleksii1994@users.noreply.github.com>
2023-11-01 09:29:10 +0000

    Deploying to gh-pages from @ Oleksii1994/goit-advancedjs-hw-01@9d0ca991f520c586a04d71bfeb3b3d6b6f560dec 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as o}from"./assets/vendor-78be7656.js";const m=document.querySelector("iframe"),e=new r(m),a=t=>{localStorage.setItem("videoplayer-current-time",t.seconds)},n=()=>localStorage.getItem("videoplayer-current-time");e.on("timeupdate",o(a,1e3));e.setCurrentTime(n());
+//# sourceMappingURL=commonHelpers2.js.map
